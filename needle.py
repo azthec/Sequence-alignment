a362f8065b49dfc7f3f454a1e5bf90581071ce87
azthec <azthec@users.noreply.github.com>
2018-03-29 14:24:14 +0100

    added constant traceback

--- needle.py
+++ needle.py
@@ -17,7 +17,7 @@ class Point:
         # return str(self.paths[0])
 
 
-def calculate_paths_const(str1, str2, match, mismatch, gap, i, j, v_matrix,):
+def calculate_paths_linear(str1, str2, match, mismatch, gap, i, j, v_matrix):
     if str1[j-1] == str2[i-1]:
         diagonal_score = v_matrix[i-1][j-1].score + match
     else:
@@ -35,8 +35,38 @@ def calculate_paths_const(str1, str2, match, mismatch, gap, i, j, v_matrix,):
         paths.append(HORIZONTAL)
     return max_score, paths
 
-def calculate_paths_affine(i, j, v_matrix):
+def calculate_paths_affine(str1, str2, match, mismatch, gap, i, j, v_matrix):
     pass
+
+def traceback_static(str1, str2, m, n, v_matrix):
+    res1 = ''
+    res2 = ''
+    while(m!=0 or n!=0): # iterate until we are at root
+        path = v_matrix[m][n].paths[0] # iterate over the first path in the point
+        if path == DIAGONAL:
+            res1 += str1[n-1]
+            res2 += str2[m-1]
+            n -= 1
+            m -= 1
+        elif path == VERTICAL:
+            res1 += '-'
+            res2 += str2[m-1]
+            m -= 1
+        elif path == HORIZONTAL:
+            res1 += str1[n-1]
+            res2 += '-'
+            n -= 1
+
+    return res1[::-1], res2[::-1] # reverse the strings
+
+
+def traceback_multiple():
+    res1 = ''
+    res2 = ''
+    while(m!=0 or n!=0): # iterate until we are at root
+        for path in v_matrix[m][n].paths: # iterate over each path to this point
+            pass
+    return res1[::-1], res2[::-1]
 
 # gap = -2, mismatch = -1, match = 1
 #     _ | a | b | c | g | e
@@ -51,7 +81,7 @@ def calculate_paths_affine(i, j, v_matrix):
 # A is known as the gap opening penalty
 # B the gap extension penalty
 # L the length of the gap
-def needleman_wunsch_constant(str1, str2, 
+def needleman_wunsch_linear(str1, str2, 
                              match, mismatch, gap):
     n = len(str1) + 1 # +1 on each for empty string origin
     m = len(str2) + 1 
@@ -70,12 +100,14 @@ def needleman_wunsch_constant(str1, str2,
 
     for i in range(1,m):
         for j in range(1,n):
-            score, paths = calculate_paths_const(str1, str2, 
+            score, paths = calculate_paths_linear(str1, str2, 
                              match, mismatch, gap, i, j, v_matrix)
             v_matrix[i][j] = Point(score, paths)
 
-
+    res1, res2 = traceback_static(str1, str2, m-1, n-1, v_matrix)
+    print(res1)
+    print(res2)
     pp = pprint.PrettyPrinter(width=len(str1), compact=True)
     pp.pprint(np.matrix(v_matrix))
 
-needleman_wunsch_constant('abcde','abcge',1,-1,-2)
+needleman_wunsch_linear('abcde','abcdef',1,-1,-2)
